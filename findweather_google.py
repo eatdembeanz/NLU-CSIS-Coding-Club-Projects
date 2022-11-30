5f3b886e80f092966794dfdfd7d93bd529a2ed51
eatdembeanz <bpage5@nl.edu>
2022-11-29 19:36:50 -0600

    adds beginning of web-scraber, gathers span tags with TemperatureValues

---
+++ findweather_google.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+import requests
+url = "https://weather.com/weather/today/l/624f0cccc10bececfa4c083056cef743837a76588790f476c9ebea44be35e51f"
+url2 = "https://www.google.com/search?q=chicago+weather"
+cont = requests.get(url)
+cont2 = requests.get(url2)
+stuff = BeautifulSoup(cont.content,features="html5lib")
+#print(stuff.prettify())
+print(stuff.find_all('span', attrs={'data-testid':'TemperatureValue'})) #looking for data-testid="TemperatureValue"
+
