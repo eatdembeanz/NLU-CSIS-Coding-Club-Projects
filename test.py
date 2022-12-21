0d9527ad317c89d2dd6eb5077e26097d9b5502ab
eatdembeanz <bpage5@nl.edu>
2022-12-20 19:39:53 -0600

    Gathers info from the official national weather service site, and prints out a number of useful weather information specifically for O'Hare Airport Chicago.
    Updated test.py with comments
    
    Added Wishlist

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import requests #requests gathers information from a given webpage
 from bs4 import BeautifulSoup #BeautifulSoup parses HTML files gathered from requests
 url = requests.get("https://forecast.weather.gov/MapClick.php?lat=41.884250000000065&lon=-87.63244999999995") #the link to this webpage takes a set of coordinates as arguments
 soup = BeautifulSoup(url.content,features="html.parser") #assigns all the content of that page to the variable "soup"
-#print(soup.prettify()) #we can use prettify() to display all html code in an easy-to-view format
+print(soup.prettify()) #we can use prettify() to display all html code in an easy-to-view format
 tds = soup.find_all("td") #finds everything on the page with the tags <td></td>, stores it as the list "tds"
 #print(tds)
 # print(tds[0])
